089e8fbb4dbeae09a5a9d190ffeaa7e181a9e292
ChrisQuestionMark <cpawel@gmail.com>
2014-04-05 00:34:24 -0400

    Install.ps1 starting to do stuff

--- Install.ps1
+++ Install.ps1
@@ -1,1 +1,19 @@
 Write-Host -foreground Green "Installing Chris' Scripts..."
+$myURL = 'http://github.com/ChrisQuestionMark/PS/tree/master/Modules'
+[regex]$regModules = 'href=\".*\/\w*\.psm1\"'
+$wc = New-Object System.Net.WebClient
+$myPage = $wc.DownloadString($myURL)
+$matches = $regModules.Matches($myPage).Value
+if ($matches -and $matches -like '*"*"*' )
+{
+    $matches | %{ 
+        $module = $_.Split('"')[1].replace('blob/','')
+        $moduleURL = 'http://raw.github.com' + $module
+        Write-Host "`t Getting $moduleURL"
+        $wc.DownloadString($moduleURL) | Invoke-Expression
+    }
+}
+
+
+
+
